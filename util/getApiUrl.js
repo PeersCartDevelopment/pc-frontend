20611d97c04fc3197ed8e65133a79a19da5201ea
HamAraAym <hamidibrahim1606@gmail.com>
2024-09-21 17:26:52 +0530

    chore: update api url

--- util/getApiUrl.js
+++ util/getApiUrl.js
@@ -2,5 +2,5 @@ export const getApiUrl = () => {
     if(window.location.pathname.includes('localhost')) {
         return 'http://localhost:5001';
     }
-    return 'https://pc-backend-17gq.onrender.com';
+    return 'https://pcbackend-7g1k.onrender.com';
 };
