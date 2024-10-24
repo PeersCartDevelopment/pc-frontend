5c1268295ac095fa688d89b14f4165233cf1b6ca
HamAraAym <hamidibrahim1606@gmail.com>
2024-10-24 13:37:53 +0530

    reverse previous code for Login

--- util/getApiUrl.js
+++ util/getApiUrl.js
@@ -2,5 +2,5 @@ export const getApiUrl = () => {
     if(window.location.pathname.includes('localhost')) {
         return 'http://localhost:5001';
     }
-    return 'https://pcbackend-7g1k.onrender.com';
+    return 'http://13.210.211.232';
 };
