c0bb231ac038af0608ac1cc453e9dd6bab4b87de
Hamid's Mac <haaray@Hamids-MacBook-Pro.local>
2024-11-11 00:00:11 +0530

    updated API URL to HTTPS

--- util/getApiUrl.js
+++ util/getApiUrl.js
@@ -2,5 +2,5 @@ export const getApiUrl = () => {
     if(window.location.pathname.includes('localhost')) {
         return 'http://localhost:5001';
     }
-    return 'http://13.210.211.232';
+    return 'https://13.210.211.232';
 };
