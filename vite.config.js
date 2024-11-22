3f31b04a77c68cde5230ee400e8c26323f808b81
HamAraAym <hamidibrahim1606@gmail.com>
2024-11-22 13:56:55 +0530

    Updated localhost from HTTP to HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://13.210.211.232', // Your backend API URL
         changeOrigin: true,
-        secure: false,
+        secure: false, // Use true if the backend API supports HTTPS, otherwise use false
       },
     },
   },
